98631b316d00f37ea46aa3ccc92ad5995e8b69b8
EldelPelo <cgvargasv@correo.udistrital.edu.co>
2019-07-01 16:32:07 -0500

    Auto

---
+++ Automatizacion/src/pro/presentancion/Automatizacion.java
@@ -0,0 +1,22 @@
+package pro.presentancion;
+
+import pro.cableado.IAutomatizacion;
+
+public class Automatizacion implements IAutomatizacion{
+
+	@Override
+	public void controlarHumedad() {
+				
+	}
+
+	@Override
+	public void controlarLuz() {
+				
+	}
+
+	@Override
+	public void controlarpH() {
+						
+	}
+	
+}
