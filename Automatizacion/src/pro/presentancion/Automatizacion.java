5316a6e1b0a0d31008d22bdf7e5e6e5f1e6653ff
EldelPelo <cgvargasv@correo.udistrital.edu.co>
2019-07-01 16:32:16 -0500

    Auto

--- Automatizacion/src/pro/presentancion/Automatizacion.java
+++ Automatizacion/src/pro/presentancion/Automatizacion.java
@@ -1,22 +1,24 @@
 package pro.presentancion;
 
+import javax.swing.JOptionPane;
+
 import pro.cableado.IAutomatizacion;
 
 public class Automatizacion implements IAutomatizacion{
 
 	@Override
 	public void controlarHumedad() {
-				
+		JOptionPane.showMessageDialog(null, "controlarHumedar");		
 	}
 
 	@Override
 	public void controlarLuz() {
-				
+		JOptionPane.showMessageDialog(null, "controlarLuz");		
 	}
 
 	@Override
 	public void controlarpH() {
-						
+		JOptionPane.showMessageDialog(null, "controlarpH");				
 	}
 	
 }
